0d552943791dafdf6ad711800f42ec8699bc972b
Sobolev Ivan <sobolev1van@yandex.ru>
2023-04-13 12:06:30 +0300

    Update 1162.cpp

--- Тимус/1162.cpp
+++ Тимус/1162.cpp
@@ -12,15 +12,15 @@ using namespace std;
  */
 
 struct exchange_point {
-  int a, b;
-  double rate, com;
+    int a, b;
+    double rate, com;
 
-  exchange_point(int a, int b, double rate, double com) {
-    this->a = a;
-    this->b = b;
-    this->rate = rate;
-    this->com = com;
-  }
+    exchange_point(int a, int b, double rate, double com) {
+        this->a = a;
+        this->b = b;
+        this->rate = rate;
+        this->com = com;
+    }
 };
 
 vector<exchange_point> changes;
@@ -29,37 +29,38 @@ vector<double> values;
 void result(bool flag) { cout << (flag ? "YES" : "NO") << endl; }
 
 double changeValue(exchange_point current_change) {
-  return (values[current_change.a] - current_change.com) * current_change.rate;
+    return (values[current_change.a] - current_change.com) * current_change.rate;
 }
 
 int main() {
-  int N, M, S;
-  double V;
-  cin >> N >> M >> S >> V;
-  values.resize(N + 1, 0.000);
-  values[S] = V;
-  int a, b;
-  double RAB, CAB, RBA, CBA;
-  for (int i = 0; i < M; i++) {
-    cin >> a >> b >> RAB >> CAB >> RBA >> CBA;
-    changes.emplace_back(exchange_point(a, b, RAB, CAB));
-    changes.emplace_back(exchange_point(b, a, RBA, CBA));
-  }
-
-  // O(n*m) m-количество обменных пунктов
-  for (int i = 0; i < N - 1; i++) {
-    for (auto &current_change : changes) {
-      values[current_change.b] =
-          max(changeValue(current_change), values[current_change.b]);
+    int N, M, S;
+    double V;
+    cin >> N >> M >> S >> V;
+    //+1 т.к. дальше добавляем по S не уменьшая на единицу
+    values.resize(N + 1, 0.000);
+    values[S] = V;
+    int a, b;
+    double RAB, CAB, RBA, CBA;
+    for (int i = 0; i < M; i++) {
+        cin >> a >> b >> RAB >> CAB >> RBA >> CBA;
+        changes.emplace_back(exchange_point(a, b, RAB, CAB));
+        changes.emplace_back(exchange_point(b, a, RBA, CBA));
     }
-  }
 
-  for (auto &i : changes) {
-    if (changeValue(i) > values[i.b]) {
-      result(true);
-      return 0;
+    // O(n*m) m-количество обменных пунктов
+    for (int i = 0; i < N - 1; i++) {
+        for (auto &current_change : changes) {
+            values[current_change.b] =
+                    max(changeValue(current_change), values[current_change.b]);
+        }
     }
-  }
-  result(false);
-  return 0;
+
+    for (auto &i : changes) {
+        if (changeValue(i) > values[i.b]) {
+            result(true);
+            return 0;
+        }
+    }
+    result(false);
+    return 0;
 }
